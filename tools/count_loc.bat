bd8a96ed9df01a00fcbdac6a21255ace3de64ba9
Robert Beckebans <robert.beckebans@gmail.com>
2026-08-13 11:10:21 +0200

    Updated count_loc.bat

--- tools/count_loc.bat
+++ tools/count_loc.bat
@@ -1,7 +1,7 @@
 cd ..
 cloc-2.04.exe ^
-  --exclude-dir=.venv,build,target,libs,extern,docs,assets ^
-  --exclude-ext=json,xml,txt ^
+  --exclude-dir=.venv,build,target,libs,third_party,ggml,extern,docs,assets ^
+  --exclude-ext=json,xml,txt,log,bat ^
   --fullpath ^
   --by-file-by-lang ^
   .
